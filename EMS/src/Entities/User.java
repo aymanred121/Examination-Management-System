2ecff55072ac89edc424f715cd4b82cd8dd2807e
el a5tabot <youssef20191700793@cis.asu.edu.eg>
2021-01-02 18:46:09 +0200

    add txt file (database script), add connection in user class

--- EMS/src/Entities/User.java
+++ EMS/src/Entities/User.java
@@ -5,6 +5,9 @@
  */
 package Entities;
 
+import java.sql.*;
+
+
 /**
  *
  * @author bizarre
@@ -24,5 +27,29 @@ public abstract class User {
         this.age = age;
     }
     
-    
+    public static void main(String[] args) {
+        
+        try{  
+            //step1 load the driver class  
+            Class.forName("oracle.jdbc.driver.OracleDriver");  
+  
+            //step2 create  the connection object  
+            Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl" ,"hr","hr");  
+  
+            //step3 create the statement object  
+            Statement stmt=con.createStatement();  
+  
+            //step4 execute query  
+            ResultSet rs=stmt.executeQuery("select * from userData");  
+            while(rs.next())  
+            System.out.println(rs.getString("USERNAME")+"  "+rs.getString("FIRST_NAME"));  
+  
+            //step5 close the connection object  
+            stmt.close();
+            con.close();  
+  
+            }
+        catch(Exception e){ System.out.println(e);}  
+  
+     }  
 }
