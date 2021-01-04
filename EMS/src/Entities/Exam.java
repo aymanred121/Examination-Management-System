9caacbd064a4409d73e7f093fe5694de86191a50
stevensameh <stevensameh1512@gmail.com>
2021-01-04 22:01:35 +0200

    Created two new classes: Course, Class and updated Exam class.

--- EMS/src/Entities/Exam.java
+++ EMS/src/Entities/Exam.java
@@ -5,13 +5,44 @@
  */
 package Entities;
 
+import java.time.*;
+import javax.xml.transform.Source;
+
 /**
  *
- * @author yn653
+ * @author yn653, Steven Sameh, Yusuf Nasser, Ayman Hassasn
  */
 public class Exam implements SqlEntity {
+    
+    /*
+     * All the attributes of the Exam Class   
+    */
+    // TBC
     final static private int maxQuestion = 50;
+    
+    final private int id;
+    final private Course course;
+    final private double totalMarks;
+    final String instructorName;
+    private LocalTime startTime, endTime;
+    private LocalDate date;
+    private Duration duration;
+    boolean validationStatus;
+    
+    /*
+     * This constructor initializes all the final attributes of the class
+    */
 
+    public Exam(int id, Course course, double totalMarks, String instructorName) {
+        this.id = id;
+        this.course = course;
+        this.totalMarks = totalMarks;
+        this.instructorName = instructorName;
+        this.endTime = startTime.plus(duration);
+    }
+  
+    
+    
     public static int getMaxQuestion() {
         return maxQuestion;
     }
@@ -34,6 +65,65 @@ public class Exam implements SqlEntity {
     @Override
     public void delete() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    /*
+     * All the setters functions of the class: startTime, date, duration, validationStatus
+     * @Note endTime will be derived from duration and startTime
+    */
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public void setDuration(Duration duration) {
+        this.duration = duration;
+    }
+
+    public void setValidationStatus(boolean validationStatus) {
+        this.validationStatus = validationStatus;
+    }
+    
+    /*
+     * All the getter functions of the Exam class
+     * @Note endTime getter function is derived from duration and startTime
+    */
+    public int getId() {
+        return id;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public double getTotalMarks() {
+        return totalMarks;
+    }
+
+    public String getInstructorName() {
+        return instructorName;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+    public boolean isValidationStatus() {
+        return validationStatus;
     }
     
     
