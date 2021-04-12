e0feb4f951c900757544aa9b3b7a7ca697b274b6
Yusuf Nasser <yesyosife@gmail.com>
2021-04-12 06:39:27 +0200

    Completed User.java class documentation and removed unused setters.

--- EMS/src/Entities/User.java
+++ EMS/src/Entities/User.java
@@ -7,29 +7,42 @@ package Entities;
 
 import java.sql.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 /**
+ * Represents the main abstract parent entity to child entities &mdash; Admin, Student and Instructor,
+ * it contains the mutual fields between the subclasses such as their username and their birthdate.
  *
  * @author Steven Sameh and AbdelAziz Mostafa
  */
-public abstract class User{
-    
-    private String username;
-    private String mobileNumber, email, firstName, middleName, lastName , password;
+
+public abstract class User {
+
+    private final String username;
+    private String mobileNumber, email, firstName, middleName, lastName, password;
     private LocalDate birthdate;
     protected boolean isFilled, isUserFilled;
-    
-    public User()
-    {
-    }
+
     /**
-     * It initializes the user with the final data. 
+     * It initializes the user with the final data.
+     *
      * @param username The username of the user
      */
+
     public User(String username) {
         this.username = username;
     }
+
+    /**
+     * Constructs a new user to be added in the database via other methods.
+     *
+     * @param username     the user's unique username
+     * @param mobileNumber the user's registered mobile number
+     * @param email        the user's contact email
+     * @param firstName    the user's first name
+     * @param middleName   the user's middle name
+     * @param lastName     the user's last name
+     * @param birthdate    the user's birthdate value
+     */
 
     public User(String username, String mobileNumber, String email, String firstName, String middleName, String lastName, LocalDate birthdate, String password) {
         this.username = username;
@@ -41,42 +54,49 @@ public abstract class User{
         this.birthdate = birthdate;
         this.password = password;
     }
-    
+
     /**
      * Represents the type of user
      */
-    public enum UserType{
+
+    public enum UserType {
         STUDENT,
         ADMIN,
         INSTRUCTOR
     }
-    
+
     /**
      * It retrieves all the data of the user from the database
      */
+
     public void fillUserData() {
         isUserFilled = true;
         Connection myConnection = SqlConnection.getConnection();
-        try{
+        try {
             PreparedStatement myStatement = myConnection.prepareStatement("select firstname, "
                     + "middlename, lastname, birthdate, mobilenumber, email"
-                + " from userdata where username = ?");
+                    + " from userdata where username = ?");
             myStatement.setString(1, username);
             ResultSet myResultSet = myStatement.executeQuery();
-            while(myResultSet.next()) {
+            while (myResultSet.next()) {
                 firstName = myResultSet.getString(1);
                 middleName = myResultSet.getString(2);
                 lastName = myResultSet.getString(3);
                 birthdate = myResultSet.getDate(4).toLocalDate();
                 mobileNumber = myResultSet.getString(5);
                 email = myResultSet.getString(6);
-            } 
+            }
             myConnection.close();
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
-    
+
+    /**
+     * Adds a new user to the database through a SQL Statement inserting
+     * all the new user's data.
+     */
+
     public void addUser() {
         Connection myConnection = SqlConnection.getConnection();
         try {
@@ -87,165 +107,139 @@ public abstract class User{
             myStatement.setString(4, lastName);
             myStatement.setDate(5, Date.valueOf(birthdate));
             myStatement.setString(6, mobileNumber);
-            myStatement.setString(7,email);
-            myStatement.setString(8,password);
+            myStatement.setString(7, email);
+            myStatement.setString(8, password);
             myStatement.executeQuery();
         } catch (Exception e) {
             System.out.println(e);
         }
     }
 
-    public void updateUser() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public void deleteUser() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
     /**
-     * it returns the UserName of the current user  
-     * @return String defining the username of the user 
+     * it returns the UserName of the current user
+     *
+     * @return String defining the username of the user
      */
+
     public String getUsername() {
-        if(!isUserFilled){
+        if (!isUserFilled) {
             this.fillUserData();
         }
         return username;
     }
-    
+
     /**
      * Checks whether the user is in the correct table in the database
-     * @author Ziad Khobeiz and Abdel-Aziz Mostafa
-     * @param username The username of the user
+     *
+     * @param username  The username of the user
      * @param tableName The corresponding table of the UserType needed to check
      * @return Boolean It determines whether the username is found in the table in the database
+     * @author Ziad Khobeiz and Abdel-Aziz Mostafa
      */
+
     private static boolean isCorrectType(String username, String tableName) {
         boolean isExisting = false;
         Connection myConnection = SqlConnection.getConnection();
-        try{
-            PreparedStatement SQLstatement;
-            SQLstatement = myConnection.prepareStatement("select count(*) from " + tableName + " where username = ?");
-            SQLstatement.setString(1, username);
-            ResultSet myResultSet = SQLstatement.executeQuery();
-            if(myResultSet.next() && myResultSet.getString(1).equals("1")) {
+        try {
+            PreparedStatement SQLStatement;
+            SQLStatement = myConnection.prepareStatement("select count(*) from " + tableName + " where username = ?");
+            SQLStatement.setString(1, username);
+            ResultSet myResultSet = SQLStatement.executeQuery();
+            if (myResultSet.next() && myResultSet.getString(1).equals("1")) {
                 isExisting = true;
             }
             myConnection.close();
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
         }
         return isExisting;
     }
-    
+
     /**
      * Determines the type of the user
-     * @author Ziad Khobeiz and Abdel-Aziz Mostafa
+     *
      * @param username The username of the user
      * @return UserType This returns the type of the user existing in the database with the corresponding username
+     * @author Ziad Khobeiz and Abdel-Aziz Mostafa
      */
+
     public static UserType getUserType(String username) {
-        
-        if(isCorrectType(username, "STUDENT")) {
+
+        if (isCorrectType(username, "STUDENT")) {
             return UserType.STUDENT;
-        } else if(isCorrectType(username, "INSTRUCTOR")) {
+        } else if (isCorrectType(username, "INSTRUCTOR")) {
             return UserType.INSTRUCTOR;
         } else {
             return UserType.ADMIN;
         }
-        
+
     }
+
     /**
      * it returns the FirstName of the User
-     * @return String defining the FirstName of the user 
+     *
+     * @return String defining the FirstName of the user
      */
+
     public String getFirstName() {
         // it fills the data of the user only not all the data of the object    
-        if(!isUserFilled){
-           this.fillUserData();
+        if (!isUserFilled) {
+            this.fillUserData();
         }
         return firstName;
     }
-    
+
     /**
-     * @author Ziad Khobeiz and Abdel-Aziz Mostafa
      * Validates that the username with the corresponding password are in the database
+     *
      * @param username The username of the user trying to login
      * @param password The password of the user trying to login
-     * @return Boolean This returns whether the username with the corresponding password exist in the database
+     * @return isValid: if the username with the corresponding password exist in the database true; false otherwise.
+     * @author Ziad Khobeiz and Abdel-Aziz Mostafa
      */
+
     public static boolean isValidUser(String username, String password) {
         boolean isValid = false;
         Connection myConnection = SqlConnection.getConnection();
-        try{
-            PreparedStatement SQLstatement;
-            SQLstatement = myConnection.prepareStatement("select count(*) from userdata where username = ? and password = ?");
-            SQLstatement.setString(1, username);
-            SQLstatement.setString(2, password);  SQLstatement.setString(1, username);
-            ResultSet myResultSet = SQLstatement.executeQuery();
-            if(myResultSet.next() && myResultSet.getString(1).equals("1")) {
+        try {
+            PreparedStatement SQLStatement;
+            SQLStatement = myConnection.prepareStatement("select count(*) from userdata where username = ? and password = ?");
+            SQLStatement.setString(1, username);
+            SQLStatement.setString(2, password);
+            SQLStatement.setString(1, username);
+            ResultSet myResultSet = SQLStatement.executeQuery();
+            if (myResultSet.next() && myResultSet.getString(1).equals("1")) {
                 isValid = true;
             }
             myConnection.close();
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
         }
         return isValid;
     }
-     /**
-     * @author Steven Sameh and Abdel-Aziz Mostafa
-     * Checks that the username exists in the database
+
+    /**
      * @param username The username of the user trying to add in database
      * @return Boolean This returns whether the username exist in the database
+     * @author Steven Sameh and Abdel-Aziz Mostafa
+     * Checks that the username exists in the database
      */
-    public static boolean isUsernameExisted( String username){
+
+    public static boolean isUsernameExisted(String username) {
         boolean isExisted = false;
         Connection myConnection = SqlConnection.getConnection();
-        try{
-            PreparedStatement SQLstatement;
-            SQLstatement = myConnection.prepareStatement("select count(*) from userdata where lower(username) = ?");
-            SQLstatement.setString(1, username);
-            ResultSet myResultSet = SQLstatement.executeQuery();
-            if(myResultSet.next() && myResultSet.getInt(1) >  0) {
+        try {
+            PreparedStatement SQLStatement;
+            SQLStatement = myConnection.prepareStatement("select count(*) from userdata where lower(username) = ?");
+            SQLStatement.setString(1, username);
+            ResultSet myResultSet = SQLStatement.executeQuery();
+            if (myResultSet.next() && myResultSet.getInt(1) > 0) {
                 isExisted = true;
             }
             myConnection.close();
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
         }
         return isExisted;
     }
-
-    public void setMobileNumber(String mobileNumber) {
-        this.mobileNumber = mobileNumber;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setBirthdate(LocalDate birthdate) {
-        this.birthdate = birthdate;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    
 }
