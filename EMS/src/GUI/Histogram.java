6b5c1fc423ae4cfbe1139313396bb2daaaa91a3b
Yusuf Nasser <yesyosife@gmail.com>
2021-04-15 20:23:09 +0200

    Update Histogram.java, Finalized the class docs
    Refactored Bar() to initHistogramData() and documented the class and its methods

--- EMS/src/GUI/Histogram.java
+++ EMS/src/GUI/Histogram.java
@@ -1,7 +1,7 @@
-
 package GUI;
+
 import javax.swing.JFrame;
-import java.sql.ResultSet;
+
 import Entities.*;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
@@ -9,37 +9,49 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.statistics.HistogramDataset;
 
 /* TODO
-*   add jcommon-1.0.0.jar
-*   and jfreechart-1.0.1.jar from dependencies\jfreechart-1.0.1\lib to the library
-*   of the project structer in order to use the freechart lib*/
+ *   add jcommon-1.0.0.jar
+ *   and jfreechart-1.0.1.jar from dependencies\jfreechart-1.0.1\lib to the library
+ *   of the project structer in order to use the freechart lib
+ */
 
 /**
- * Draw a HistogramChart using JfreeChart lib
+ * Draws a HistogramChart using JfreeChart library
+ *
  * @author Ayman Hassan, Yusuf Nasser, Youssef Nader
  */
 
 public class Histogram extends JFrame {
-    private double[] data;
-    private ResultSet myResultSet;
+    private final double[] data;
     private JFreeChart HistogramChart;
-    private String title, xAxis, yAxis;
+    private final String title, xAxis, yAxis;
+
+    /**
+     * Constructs a Histogram Chart to exemplify a certain dataset in a graphical way to be used as
+     * a statistical representation.
+     *
+     * @param data  The data upon which the histogram is based
+     * @param title The title of the histogram — the meaning of the relation represented between X and Y axes values
+     * @param xAxis xAxis Label — the element represented by X Axis values
+     * @param yAxis yAxis Label — the element represented by Y Axis values
+     */
+
     public Histogram(double[] data, String title, String xAxis, String yAxis) {
         this.data = data;
         this.xAxis = xAxis;
         this.yAxis = yAxis;
         this.title = title;
-        Bar();
+        initHistogramData();
     }
 
     /**
-     * Create a HistogramChart by initializing the title and the AxisLable on both X and Y, then
-     * passing the dataset from createDataset method, and setting the direction of the drawing.
+     * Creates and initialize the histogram's dataset then initializes
+     * the JFreeChart instance in order to create the histogram.
      */
 
-    private void Bar(){
-        HistogramDataset dataset= new HistogramDataset();
-        dataset.addSeries("key",data,20);
-         HistogramChart = ChartFactory.createHistogram(
+    private void initHistogramData() {
+        HistogramDataset dataset = new HistogramDataset();
+        dataset.addSeries("Grades", data, 10);
+        HistogramChart = ChartFactory.createHistogram(
                 title,
                 xAxis,
                 yAxis,
@@ -48,22 +60,28 @@ public class Histogram extends JFrame {
                 true, true, false);
     }
 
+    /**
+     * Returns the Histogram Chart
+     *
+     * @return the value of HistogramChart property
+     */
+
     public JFreeChart getHistogramChart() {
         return HistogramChart;
     }
 
     /**
      * retrieves and returns the students' exams data upon which the histogram is based
+     *
      * @param exam — the exam to retrieve its students data
      * @return array of doubles contains the data
      */
 
-    public static double[] HistogramData(Exam exam){
+    public static double[] HistogramData(Exam exam) {
         int studentIndex = 0, studentCount = exam.getStudentCount();
         double[] studentMarksData = new double[studentCount];
 
-        for (Student student :  exam.getExamClass().getStudents() )
-        {
+        for (Student student : exam.getExamClass().getStudents()) {
             studentMarksData[studentIndex] = exam.getStudentMark(student.getUsername());
             studentIndex++;
         }
